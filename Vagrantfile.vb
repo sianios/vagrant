fc9c13bc54f6fe2e305760a152f3df5a3af5c0f9
sianios <m@sianios.com>
2017-11-05 13:04:33 +0200

    added back vagrant vb file

---
+++ Vagrantfile.vb
@@ -0,0 +1,21 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+$zoneinfo = <<SCRIPT
+ln -sf /usr/share/zoneinfo/Asia/Nicosia /etc/localtime
+SCRIPT
+
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "debian/stretch64"
+  #config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.provider "virtualbox" do |vb|
+  #  vb.gui = true
+    vb.customize ["modifyvm", :id, "--memory", "2048", "--cpus", "2"]
+  end
+  
+  config.vm.hostname = "stretch64"
+  config.vm.provision "shell", inline: $zoneinfo
+
+end
